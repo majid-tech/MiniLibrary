f572e2bdb35123b46e6519516a351393bd8cef9b
Mohammed Majid <majidmohammed044@gmail.com>
2025-01-09 10:01:57 +0000

    added object constructor

--- interactivity.js
+++ interactivity.js
@@ -1,7 +1,14 @@
 const myLibrary = [];
 
-function Book() {
+function Book(title, author,pages,read) {
   // the constructor...
+this.title = title,
+this.author = author,
+this.pages = pages,
+this.read = read,
+this.info = function() {
+  return `${title} by ${author}, ${pages} pages, ${read}`;
+}
 }
 
 function addBookToLibrary() {
